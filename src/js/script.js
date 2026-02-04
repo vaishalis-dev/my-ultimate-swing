ad74b3d10dcbcb0d91a4681e688c3778cd6a2f49
vaishali <vaishalisharma.vt@gmail.com>
2026-02-04 17:48:39 +0530

    Do some improvements like updated the background of the first section and fix the icons of the last sections

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,55 @@
-const rightScrollContainer = document.getElementById("scrollContainer");
-const steps = [...rightScrollContainer.querySelectorAll(".step-image")];
+const leftSticky = document.getElementById("left-sticky-wrapper");
+const rightScroll = document.getElementById("right-scroll-container");
+const firstStep = rightScroll.querySelector("[data-anchor]");
+const scrollContainer = document.getElementById("scrollContainer");
+const steps = [...document.querySelectorAll(".step-image")];
 const texts = [...document.querySelectorAll(".text-item")];
+
+function setRightPadding() {
+  const vh = window.innerHeight;
+  const stepHeight = firstStep.getBoundingClientRect().height;
+
+  const centerPadding = vh / 2 - stepHeight / 2;
+
+  rightScroll.style.paddingTop = `${centerPadding}px`;
+  // rightScroll.style.paddingBottom = `${centerPadding}px`;
+  // rightScroll.style.paddingBottom = `${centerPadding + vh / 2}px`;
+
+  const leftHeight = leftSticky.getBoundingClientRect().height;
+  leftSticky.style.top = `${vh / 2 - leftHeight / 2}px`;
+}
+
+// initial
+setRightPadding();
+
+// on resize
+// window.addEventListener("resize", setRightPadding);
+
+let resizeRAF;
+
+window.addEventListener("resize", () => {
+  if (resizeRAF) return;
+
+  resizeRAF = requestAnimationFrame(() => {
+    setRightPadding();
+    resizeRAF = null;
+  });
+});
+
+// function catchScroll() {
+//   console.log("scrolling......");
+//   const scrollTop = scrollContainer.scrollTop; // current scroll position
+//   const scrollHeight = scrollContainer.scrollHeight; // total scrollable height
+//   const clientHeight = scrollContainer.clientHeight; // visible height
+
+//   // console.log(scrollTop, scrollHeight, clientHeight);
+// }
 
 let activeIndex = 0;
 
 function setActiveText(index) {
+  // if (index === activeIndex) return;
+
   texts.forEach((text, i) => {
     text.classList.remove("active", "prev", "next");
 
@@ -32,8 +77,9 @@ function setActiveText(index) {
   activeIndex = index;
 }
 
-function onRightScroll() {
-  const containerRect = rightScrollContainer.getBoundingClientRect();
+function onScroll() {
+  console.log("scrolling.....");
+  const containerRect = scrollContainer.getBoundingClientRect();
   const containerCenter = containerRect.top + containerRect.height / 2;
 
   let closestIndex = 0;
@@ -42,7 +88,6 @@ function onRightScroll() {
   steps.forEach((step, index) => {
     const rect = step.getBoundingClientRect();
     const stepCenter = rect.top + rect.height / 2;
-
     const distance = Math.abs(stepCenter - containerCenter);
 
     if (distance < minDistance) {
@@ -51,61 +96,17 @@ function onRightScroll() {
     }
   });
 
-  if (closestIndex !== activeIndex) {
-    setActiveText(closestIndex);
+  setActiveText(closestIndex);
+}
+
+// scrollContainer.addEventListener("scroll", onScroll, { passive: true });
+let ticking = false;
+scrollContainer.addEventListener("scroll", () => {
+  if (!ticking) {
+    requestAnimationFrame(() => {
+      onScroll();
+      ticking = false;
+    });
+    ticking = true;
   }
-}
-
-// Listen ONLY to right container scroll
-rightScrollContainer.addEventListener("scroll", onRightScroll, {
-  passive: true,
 });
-
-// Initial state when entering section
-setActiveText(0);
-
-const leftSticky = document.getElementById("left-sticky-wrapper");
-const rightScroll = document.getElementById("right-scroll-container");
-const firstStep = rightScroll.querySelector("[data-anchor]");
-
-function setRightPadding() {
-  const viewportHeight = window.innerHeight;
-  const stepHeight = firstStep.offsetHeight;
-
-  const paddingY = viewportHeight / 2 - stepHeight / 2;
-
-  rightScroll.style.paddingTop = `${paddingY}px`;
-  // rightScroll.style.paddingBottom = `${paddingY + vh / 2}px`;
-  rightScroll.style.paddingBottom = `${paddingY}px`;
-
-  const leftHeight = leftSticky.offsetHeight;
-  const top = viewportHeight / 2 - leftHeight / 2;
-
-  leftSticky.style.top = `${top}px`;
-}
-
-// function setRightPadding() {
-//   const vh = window.innerHeight;
-//   const stepHeight = firstStep.offsetHeight;
-
-//   // Center step
-//   const centerPadding = vh / 2 - stepHeight / 2;
-
-//   // Apply to right scroll
-//   rightScroll.style.paddingTop = `${centerPadding}px`;
-
-//   // IMPORTANT PART 👇
-//   // Give last image time before snap
-//   rightScroll.style.paddingBottom = `${centerPadding + vh / 2}px`;
-
-//   // Left stays centered
-//   const leftHeight = leftSticky.offsetHeight;
-//   leftSticky.style.top = `${vh / 2 - leftHeight / 2}px`;
-// }
-
-
-// initial
-setRightPadding();
-
-// on resize
-window.addEventListener("resize", setRightPadding);
